1b87f52bde0dfa0894ff1908f71672addde82de0
aliana <ac4820@princeton.edu>
2026-03-21 20:26:49 -0400

    first draft

---
+++ symtablelist.c
@@ -0,0 +1,143 @@
+
+#include "symtable.h"
+
+struct symTableBinding 
+{
+    const char *key; 
+    const void  *value;
+    struct symTableBinding *psNextBinding;
+};
+
+struct Table 
+{
+    struct symTableBinding* firstBinding;
+    int size;
+};
+
+SymTable_T SymTable_new(void) 
+{                     
+    SymTable_T newSymTable;
+    newSymTable = malloc(sizeof(struct Table)); 
+    if (newSymTable == NULL) return NULL;
+    newSymTable->firstBinding = NULL;
+    nweSymTable->size = 0;
+    return newSymTable;
+}
+
+void SymTable_free(SymTable_T oSymTable) 
+{
+    struct symTableBinding *psCurrentBinding
+    struct symTableBinding *psNextBinding               
+    assert(oSymTable != NULL);
+    for (psCurrentBinding = oSymTable->firstBinding;
+         psCurrentBinding != NULL;          
+         psCurrentBinding = psNextBinding)             
+    {
+        psNextBinding = psCurrentBinding->psNextBinding;
+        free(psCurrentBinding)
+    }
+        free(oSymTable);
+}
+
+
+size_t SymTable_getLength(SymTable_T oSymTable) 
+{   
+    return oSymTable->size;
+}
+
+int SymTable_put(SymTable_T oSymTable, const char *pcKey, const void *pvValue) 
+{
+    struct symTableBinding *psCurrentBinding;
+    assert(oSymTable != NULL);
+    for (psCurrentBinding = oSymtable->firstBinding; 
+         psCurrentBinding != NULL;
+         psCurrentBinding = psCurrentBinding->psNextBinding) 
+    {
+        if (strcmp(pcKey, psCurrentBinding->key) == 0) {    
+            return 0;
+        }
+    }
+    struct symTableBinding *newBinding = malloc(sizeof(*newBinding));
+    newBinding->key = (const char*)malloc(strlen(key)+1);
+    strcpy((char*)p->key, key);
+    newBinding->value = pvValue;      
+    newBinding->psNextBinding = oSymTable->firstBinding;
+    oSymTable->firstBinding = newBinding;
+}
+
+void *SymTable_replace(SymTable_T oSymTable, const char *pcKey, const void *pvValue)
+{
+    struct symTableBinding *psCurrentBinding;
+    void *oldValue;
+    assert(oSymTable != NULL);
+    for (psCurrentBinding = oSymTable->firstBinding;
+         psCurrentBinding != NULL;
+         psCurrentBinding = psCurrentBinding->psCurrentNext)
+    {
+        if(strcmp(pcKey, psCurrentBinding->key) == 0){
+            oldValue = psCurrentBinding->value;         
+            psCurrentBinding->value = pvValue;         
+            return oldValue;                         
+        }
+    }
+        return NULL;
+}
+
+int SymTable_contains(SymTable_T oSymTable, const char *pcKey)
+{
+    struct symTableBinding *psCurrentBinding;
+    for (psCurrentBinding = oSymTable->firstBinding;
+         psCurrentBinding != NULL;
+         psCurrentBinding = psCurrentBinding->psCurrentNext)
+    {
+        if(strcmp(pcKey, psCurrentBinding->key) == 0){
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+void *SymTable_get(SymTable_T oSymTable, const char *pcKey);
+{
+    struct symTableBinding *psCurrentBinding;
+    for (psCurrentBinding = oSymTable->firstBinding;
+         psCurrentBinding != NULL;
+         psCurrentBinding = psCurrentBinding->psCurrentNext)
+    {
+        if(strcmp(pcKey, psCurrentBinding->key) == 0)
+        return pcCurrentBinding->value;                    
+    }
+        return NULL;
+}
+
+void *SymTable_remove(SymTable_T oSymTable, const char *pcKey);
+{
+    struct symTableBinding *psCurrentBinding;
+    struct symTableBinding *psPreviousBinding = NULL;
+
+    void *oldValue;
+    for (psCurrentBinding = oSymTable->firstBinding;
+         psCurrentBinding != NULL;
+         psPreviousBinding = psCurrentBinding,
+         psCurrentBinding = psCurrentBinding->psNextBinding;
+        )
+    {
+        if(strcmp(pcKey, psCurrentBinding->key) == 0) {
+            if(psPreviousBinding == NULL)
+            {
+                oSymTable->first = psCurrentBinding->psCurrentNext;
+            } else {       
+                psPreviousBinding->psNextBinding = psCurrentBinding->psNextBinding;
+            }
+            oldValue = psCurrentBinding->value;
+            free(psCurrentBinding);
+            return oldValue;
+    }
+    }
+    return NULL;
+}
+/*
+void SymTable_map(SymTable_T oSymTable,
+     void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
+     const void *pvExtra);
+*/
